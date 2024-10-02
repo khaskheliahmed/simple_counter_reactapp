2f5ae405e6d3b8472581bf2e50555ccbe0e6c27b
khaskheliahmed <ahmadkhaskheli91@gmail.com>
2024-10-02 10:25:53 +0500

    change

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  
+
+let [counter , setcounter ]= useState(12)
+
+  // let counter = 15;
+
+  const addvalue = () =>{
+    console.log("value add" , counter)
+
+    counter = counter +1;
+       
+    setcounter(counter)
+  }
+
+  const removeValue = () =>{
+
+  
+
+    if( counter >= 1){
+      console.log("remove value ", counter)
+      counter = counter - 1;
+    setcounter(counter) 
+    }
+  }
+  
+
+  return (
+    <>
+    <h1>hello chaa hall bhaw </h1>
+     <h2>counter value : {counter}</h2>
+     
+   
+    <button className='btn' onClick={addvalue} >addValue</button>
+    <button className='btn2'onClick={removeValue} >remove Value</button>
+    </>
+  )
+}
+
+export default App
